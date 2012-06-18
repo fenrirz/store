3049815728fd54dc0fdfb704f8d74d2e9c9e3a8c
Fenrirz <fenrirzzzzz#gmail.com>
2012-06-18 14:56:35 +0700

    BU 18June

--- last/app/helpers/application_helper.rb
+++ last/app/helpers/application_helper.rb
@@ -11,4 +11,17 @@ module ApplicationHelper
     end
   end
 
+  def list_categories
+    str = "<ul>"
+    Category.where(["parent_id IS NULL"]).each do |parent_category|
+      str += "<li>#{parent_category.name}</li>"
+      str += "<ul>"
+      Category.where("parent_id = ?", parent_category.id).each do |category|
+        str += "<li>#{category.name}</li>"
+      end
+      str += "</ul>"
+    end
+    return str += "</ul>"
+  end
+
 end
